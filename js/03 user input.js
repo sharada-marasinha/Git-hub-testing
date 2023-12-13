fe32263041ec30b674850cea2a078aa1fd3d93b6
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2023-12-13 13:28:37 +0530

    adding user input module

---
+++ js/03 user input.js
@@ -0,0 +1,20 @@
+// 🎤 Welcome to the User Input Module! Get ready for some interactive fun! 🕹️🎉
+
+// 🤖 Let's ask the user for their name. Prepare for a friendly chat! 👋😊
+let userName = prompt("Hey there! What's your name?");
+alert(`Hello, ${userName}! Welcome to the User Input Module! 🎉🚀`);
+
+// 🍕 Time for a pizza order! Ask the user for their favorite topping. 🍍🍕
+let favoriteTopping = prompt(`${userName}, what's your favorite pizza topping?`);
+alert(`Great choice, ${userName}! ${favoriteTopping} pizza is the best! 🍕🌟`);
+
+// 🏆 Get ready for a challenge - ask the user for their lucky number! 🍀🔢
+let luckyNumber = prompt(`${userName}, what's your lucky number?`);
+let nextNumber = Number(luckyNumber) + 1; // Convert to number for addition
+alert(`Wow, ${userName}! Your lucky number is ${luckyNumber}, and the next one is ${nextNumber}! 🎱🌈`);
+
+// 🎭 It's showtime! Ask the user to share their favorite emoji. 🌟😄
+let favoriteEmoji = prompt(`${userName}, what's your absolute favorite emoji?`);
+alert(`Nice choice, ${userName}! ${favoriteEmoji} is the epitome of emoji perfection! 🎭💖`);
+
+// 🚀 Congratulations! You've successfully interacted with the User Input Module! 🎉🥳
