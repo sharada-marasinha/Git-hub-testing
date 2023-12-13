8405ca2d24d34f211c4255d0673d145fb194a060
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2023-12-13 13:17:56 +0530

    adding  Variables Module

---
+++ js/01 variables.js
@@ -0,0 +1,45 @@
+// 🚀 Welcome to the Variables Module! 🚀
+
+// Let's declare some variables:
+
+// 🍎 An apple a day keeps the errors away! 🍏
+let fruit = "apple";
+
+// ☕️ Time for a coffee break! ☕️
+let beverage = "coffee";
+
+// 🎲 Roll the dice! 🎲
+let diceNumber = 6;
+
+// 🌈 Colors of the rainbow! 🌈
+let color = "blue";
+
+// Now, let's do some variable gymnastics:
+
+// 🔄 Reassigning variables is like changing outfits! 🧥
+fruit = "banana";
+
+// 🤖 Variables can be as dynamic as a robot dance! 🕺
+let dynamicVariable = 42;
+dynamicVariable = "I can be a string too!";
+
+// 💫 Let's mix and match data types! 🍹
+let mixedVariable = "hello" + "world";
+mixedVariable = 42 + " is the answer";
+
+// 🚨 Watch out for the tricky typeof operator! 🕵️‍♂️
+let mysteriousVariable = true;
+console.log(typeof mysteriousVariable); // Spoiler alert: It's a boolean!
+
+// 🌍 Variables can travel across functions! 🚢
+function travelFunction() {
+  let localVariable = "I'm local!";
+  console.log(localVariable);
+}
+
+// 🧩 Commenting out variables is like hiding them in a puzzle! 🧩
+// let hiddenVariable = "You can't see me!";
+
+// 🎉 Hooray! You've mastered the Variables Module! 🎉
+
+//@sharada-marasinha
