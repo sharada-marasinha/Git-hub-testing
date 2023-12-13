401975ec0d8d32f4afeda6b863bc118209243558
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2023-12-13 13:58:06 +0530

    adding Type Conversion Module

---
+++ js/04 type conversion.js
@@ -0,0 +1,32 @@
+// 🎭 Welcome to the Type Conversion Module! Let's explore the magical world of type conversions! ✨
+
+// 🧙‍♂️ String to Number: Converting strings to numeric values!
+let stringNumber = "42";
+let convertedNumber = Number(stringNumber); // Result: 42
+
+// 🧚 Number to String: Transforming numeric values to strings!
+let numericValue = 99;
+let convertedString = String(numericValue); // Result: "99"
+
+// 🧜‍♀️ Boolean to String: Converting boolean values to strings!
+let isSunny = true;
+let convertedBoolToString = String(isSunny); // Result: "true"
+
+// 🧞‍♂️ String to Boolean: Transforming strings to boolean values!
+let rainyDay = "false";
+let convertedStringToBool = Boolean(rainyDay); // Result: true (if the string is "false")
+
+// 🦄 Numeric to Boolean: Converting numeric values to booleans!
+let temperature = 25;
+let convertedNumberToBool = Boolean(temperature); // Result: true (non-zero values are truthy)
+
+// 🚀 Displaying Results: Let's see the magic!
+console.log("Converted String to Number:", convertedNumber);
+console.log("Converted Number to String:", convertedString);
+console.log("Converted Boolean to String:", convertedBoolToString);
+console.log("Converted String to Boolean:", convertedStringToBool);
+console.log("Converted Number to Boolean:", convertedNumberToBool);
+
+// 💡 Bonus Tip: Use the "+" operator for concise string-to-number conversion!
+
+// 🌟 Congratulations! You've mastered the Type Conversion Module! 🌟
