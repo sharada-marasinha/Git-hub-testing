6444e22dcaecfec6fd26ef48e8a60bbf96d19a2f
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2023-12-13 13:54:43 +0530

    update user input module

--- js/03 user input.js
+++ js/03 user input.js
@@ -1,19 +1,19 @@
 // 🎤 Welcome to the User Input Module! Get ready for some interactive fun! 🕹️🎉
 
-// 🤖 Let's ask the user for their name. Prepare for a friendly chat! 👋😊
+// 🤖 Asking for User's Name: Let's start with a friendly chat!
 let userName = prompt("Hey there! What's your name?");
 alert(`Hello, ${userName}! Welcome to the User Input Module! 🎉🚀`);
 
-// 🍕 Time for a pizza order! Ask the user for their favorite topping. 🍍🍕
+// 🍕 Ordering Pizza: Let's ask for the user's favorite topping!
 let favoriteTopping = prompt(`${userName}, what's your favorite pizza topping?`);
 alert(`Great choice, ${userName}! ${favoriteTopping} pizza is the best! 🍕🌟`);
 
-// 🏆 Get ready for a challenge - ask the user for their lucky number! 🍀🔢
+// 🏆 Lucky Number Challenge: Ask the user for their lucky number!
 let luckyNumber = prompt(`${userName}, what's your lucky number?`);
 let nextNumber = Number(luckyNumber) + 1; // Convert to number for addition
 alert(`Wow, ${userName}! Your lucky number is ${luckyNumber}, and the next one is ${nextNumber}! 🎱🌈`);
 
-// 🎭 It's showtime! Ask the user to share their favorite emoji. 🌟😄
+// 🎭 Favorite Emoji: Time to express! Ask for the user's favorite emoji.
 let favoriteEmoji = prompt(`${userName}, what's your absolute favorite emoji?`);
 alert(`Nice choice, ${userName}! ${favoriteEmoji} is the epitome of emoji perfection! 🎭💖`);
 
