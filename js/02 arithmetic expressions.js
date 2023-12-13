036f445856e672824ace389357162c914a1ac15b
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2023-12-13 13:23:41 +0530

    adding arithmetic expressions module

---
+++ js/02 arithmetic expressions.js
@@ -0,0 +1,32 @@
+// 🎲 Welcome to the Arithmetic Expressions Module! Let's do some math magic! 🎩✨
+
+// 🍎 An apple plus an apple equals... an applesauce? 🍏🥄
+let apples = 2 + 2; // Result: 4
+
+// ☕️ Adding coffee cups because why not? ☕️☕️
+let coffeeCups = 3 + 2; // Result: 5
+
+// 🌈 Multiplying rainbows! Each color times 3. 🌈🌈🌈
+let rainbowColors = 7 * 3; // Result: 21
+
+// 🎲 Rolling a dice and adding a bonus! 🎲+1️⃣
+let diceRoll = 4 + 1; // Result: 5
+
+// 🚀 Exponential fun - launching to the power of 2! 🚀²
+let exponentialPower = 2 ** 2; // Result: 4
+
+// 🤔 Dividing pizza slices among friends! 🍕👭
+let pizzaSlices = 8 / 2; // Result: 4
+
+// 🚇 Subway sandwich math - stacking layers! 🥖🥓🧀🥗
+let subwayLayers = 3 + 2 + 1; // Result: 6
+
+// 🌐 Modulus party - finding the remainder of a division! 🎉🎊
+let modulusResult = 15 % 4; // Result: 3
+
+// 🤯 Complex expressions - order of operations challenge! 🧠
+let complexExpression = 2 + 3 * 4 - (8 / 2); // Result: 14
+
+// 💡 Bonus tip: Parentheses are your math superpower! 💪
+
+// 🌟 Congratulations! You've mastered the Arithmetic Expressions Module! 🌟
